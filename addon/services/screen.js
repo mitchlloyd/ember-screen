391d685581da85f54fea1625b793b3539a75c82d
Mitch Lloyd <mitch.lloyd@gmail.com>
2017-03-12 12:52:47 -0700

    Switch to global FastBoot detection

--- addon/services/screen.js
+++ addon/services/screen.js
@@ -1,13 +1,14 @@
+/* globals FastBoot */
 import Ember from 'ember';
 import BrowserWindow from 'ember-screen/window/browser';
 import NullWindow from 'ember-screen/window/null';
 
-const { computed, get, getOwner } = Ember;
+const isFastBoot = typeof FastBoot !== 'undefined';
+const WindowClass = isFastBoot ?  NullWindow : BrowserWindow;
 
 export default Ember.Service.extend({
   init() {
     this._super(...arguments);
-    let WindowClass = this.get('isFastBoot') ? NullWindow : BrowserWindow;
     this.win = new WindowClass();
     this.win.onSizeUpdate(this.handleResize.bind(this));
   },
@@ -23,25 +24,5 @@ export default Ember.Service.extend({
 
   stubMediaFeatures(features) {
     this.win.stubMediaFeatures(features);
-  },
-
-  isFastBoot: computed(function() {
-    if (!getOwner) {
-      // Ember.getOwner is available as of Ember 2.3, while FastBoot requires 2.4. So just return false...
-      return false;
-    }
-
-    let owner = getOwner(this);
-    if (!owner) {
-      return false;
-    }
-
-    let fastboot = owner.lookup('service:fastboot');
-    if (!fastboot) {
-      return false;
-    }
-
-    return get(fastboot, 'isFastBoot');
-  })
+  }
 });
-
