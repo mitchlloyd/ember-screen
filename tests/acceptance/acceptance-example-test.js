e6c26a98da81c9b083a4d14d8aa87d7e2d905600
Mitch Lloyd <mitch.lloyd@gmail.com>
2018-08-05 20:01:42 -0700

    Add back 2.4 and 2.8 lts versions to ember-try

--- tests/acceptance/acceptance-example-test.js
+++ tests/acceptance/acceptance-example-test.js
@@ -1,18 +1,25 @@
 import { visit } from '@ember/test-helpers';
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
+import { run } from '@ember/runloop';
 
 module('Acceptance | temp', function(hooks) {
   setupApplicationTest(hooks);
 
   test('forcing media features in an acceptance test', async function(assert) {
     let screen = this.owner.lookup('service:screen');
-    screen.stubMediaFeatures({ width: '1px' });
+
+    run(() => {
+      screen.stubMediaFeatures({ width: '1px' });
+    });
 
     await visit('/');
 
     assert.ok(screen.get('isExtraSmallAndDown'), "Media is matched");
-    screen.stubMediaFeatures({ width: '1000px' });
+
+    run(() => {
+      screen.stubMediaFeatures({ width: '1000px' });
+    });
 
     assert.notOk(screen.get('isExtraSmallAndDown'), "Media is not matched");
   });
