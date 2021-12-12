9278468bd7f17933da4e94f7665bf99cb4be8d1b
Mitch Lloyd <mitch.lloyd@gmail.com>
2021-12-11 20:43:32 -0500

    Fix test consumer component

--- tests/dummy/app/components/es-display.js
+++ tests/dummy/app/components/es-display.js
@@ -2,5 +2,5 @@ import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 
 export default class EsDisplay extends Component {
-  screen = service();
+  @service screen;
 }
